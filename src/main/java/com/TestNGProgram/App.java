ef5f2f16fca403846f36320e3294dbb1aa9dfa08
KumarGarurav <KumarGarurav@192.168.1.198>
2025-05-13 16:35:04 +0530

    File added

--- src/main/java/com/TestNGProgram/App.java
+++ src/main/java/com/TestNGProgram/App.java
@@ -6,5 +6,6 @@ package com.TestNGProgram;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is all about testing");
     }
 }
